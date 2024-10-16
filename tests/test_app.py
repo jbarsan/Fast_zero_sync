1524836285eb39171040523dbbb86d8c5b58df50
João Carlos Barsan <joao_barsan@outlook.com>
2024-10-16 18:27:27 -0300

    Aula 02

--- tests/test_app.py
+++ tests/test_app.py
@@ -9,4 +9,4 @@ def test_root_deve_retornar_ok_e_ola_mundo():
     client = TestClient(app)  # Arrange (Organização)
     response = client.get('/')  # Act (Ação)
     assert response.status_code == HTTPStatus.OK  # Assert (Afirmar)
-    assert response.json() == {'message': 'Fala cambada!!'}  # Assert
+    assert response.json() == {'message': 'Fala cambada!!'}  # Assert 
